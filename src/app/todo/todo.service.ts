eb0e427fa58d0d1ccda6bec0a22af8c87f94b537
shenni <shenni@softia.fr>
2020-01-29 14:12:00 +0100

    WIP todo service

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-
 const TODOS = [
   { title: 'Install Angular CLI', isDone: true },
   { title: 'Style app', isDone: true },
